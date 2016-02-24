686aa2e3d90a4a8bfca02cf64423d05660bd6d1b
Ridhwan <ridhwancode@gmail.com>
2016-02-24 17:23:02 +0000

    put to edit stuff online and then send back here as it is harder to edit in pi

--- ptest.py
+++ ptest.py
@@ -2,11 +2,8 @@ import bot_movement as bm
 import ultrasonic as u
 
 try:
-	while True:
-        	if u.ultra() < 20:
-                	bm.turn_right()
-        	else:
-                	bm.up()
+	bm.left()
+	bm.down()
 except:
         print "cleaning ptest.py"
 finally:
