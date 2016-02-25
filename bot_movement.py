a2c591ab292e33cfc0222fdc03d82e6a7ac55a19
Ridhwanluthra <luthraridhwan@gmail.com>
2016-02-26 02:32:51 +0530

    added the ultrasonic test and the space where the anomaly will be handled and then the code would begin to function normally just the way it was still need to calliberate

--- bot_movement.py
+++ bot_movement.py
@@ -2,6 +2,8 @@ from gpiozero import Motor
 #from gpiozero.Pin import
 #import RPi.GPIO as GPIO
 from encoders import d_move, refresh
+from ultrasonic import ultra
+from anomaly import check
 
 ml = Motor(2, 3)
 mr = Motor(14, 15)
@@ -16,20 +18,16 @@ the bot can make only 90 degree turns
 
 
 direction = 'n' # n,e,w,s for different locations that it is facing
-usfront = 20
 
 try:
-	# ADD SOMETHING TO DO WHEN US IS LESS THAN 5;
     def forward():
-    	if usfront <= 5:
-            print "error"
-    	else:
-            ml.forward(0.5)
-            mr.forward(0.5)
+        ml.forward(0.5)
+        mr.forward(0.5)
 
     def sstop():
         ml.stop()
         mr.stop()
+
     def turn_left():
         while d_move()[0] < 11 and d_move()[1] < 11:
             ml.backward(0.5)
@@ -46,31 +44,47 @@ try:
 
     def up():
         look_up()
-        while d_move()[0] <= 20 and d_move()[1] <= 20:
-            forward()
-        sstop()
-        refresh()
+        if check() == True:
+	        while d_move()[0] <= 20 and d_move()[1] <= 20:
+	            forward()
+        	sstop()
+        	refresh()
+        	return True
+        else:
+        	return False
 
     def left():
         look_left()
-        while d_move()[0] <= 20 and d_move()[1] <= 20:
-            forward()
-        sstop()
-        refresh()
+        if check() == True:
+	        while d_move()[0] <= 20 and d_move()[1] <= 20:
+	            forward()
+	        sstop()
+	        refresh()
+	        return True
+	    else:
+	    	return False
 
     def right():
         look_right()
-        while d_move()[0] <= 20 and d_move()[1] <= 20:
-            forward()
-        sstop()
-        refresh()
+        if check() == True
+	        while d_move()[0] <= 20 and d_move()[1] <= 20:
+	            forward()
+	        sstop()
+	        refresh()
+	        return True
+	    else:
+	    	return False
 
     def down():
         look_down()
-        while d_move()[0] <= 20 and d_move()[1] <= 20:
-            forward()
-        sstop()
-        refresh()
+        if check() == True:
+	        while d_move()[0] <= 20 and d_move()[1] <= 20:
+	            forward()
+	        sstop()
+	        refresh()
+	        return True
+	    else:
+	    	return False
 
     def look_up():
         global direction
