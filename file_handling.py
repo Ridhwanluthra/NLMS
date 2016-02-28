6fa2f7c5192df9baf134df81ce750749294acf2e
Ridhwanluthra <ridhwancode@gmail.com>
2016-02-28 23:23:21 +0530

    file handling stuff

--- file_handling.py
+++ file_handling.py
@@ -8,24 +8,25 @@ def write_in_file(digit, x, y):
         myfile.write('\n')
 
 def getcoords(digit):
-    digit_as_char=str(digit)
-    f=open(file_name, 'r')
-    list_of_lines=f.readlines()
+    digit_as_char = str(digit)
+    f = open(file_name, 'r')
+    list_of_lines = f.readlines()
+    print list_of_lines
     for i in range (len(list_of_lines)):
-        if list_of_lines[i][0]==digit_as_char:
+        if list_of_lines[i][0] == digit_as_char:
             return [list_of_lines[i][1], list_of_lines[i][2]]
-            pass
     else:
         print "Digit not found"
         return [-1]
 def getdigit(x, y):
     x_as_str=str(x)
     y_as_str=str(y)
-    f=open(file_name, 'r')
-    list_of_lines=f.readlines()
-    for i in range (len(list_of_lines)):
-        if list_of_lines[i][1]==x_as_str and list_of_lines[i][2]==y_as_str:
-            return digit
+    f = open(file_name, 'r')
+    list_of_lines = f.readlines()
+    print list_of_lines
+    for i in range(len(list_of_lines)):
+        if list_of_lines[i][1] == x_as_str and list_of_lines[i][2] == y_as_str:
+            return list_of_lines[i][0]
     else:
         print "No number at this coordinate"
         return -1
