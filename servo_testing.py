39a42088e5d22ad6d25d086c841febb24079c142
pankajisadeveloper <pankajisadeveloper>
2016-03-03 00:53:34 +0530

    servo

---
+++ servo_testing.py
@@ -0,0 +1,12 @@
+import anomaly as al
+from time import sleep
+
+print "PICKING"
+al.pick_the_block()
+sleep(10)
+print "DROPPING"
+al.drop_the_block()
+sleep(10)
+print "RESTING"
+al.rest()
+sleep(10)
