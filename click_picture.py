1e0b87daa78283bc80d4f889bf4b4fef825e41a2
pankajisadeveloper <pankajisadeveloper>
2016-03-03 00:24:35 +0530

    Click_picture semi final

--- click_picture.py
+++ click_picture.py
@@ -25,58 +25,58 @@ def clicked():
         camera.resolution=(300,200)
         camera.capture(direc)
 
-    #analysis of image
-# load the image
-image = cv2.imread(direc)
+            #analysis of image
+        # load the image
+        image = cv2.imread(direc)
 
-img = image
-img=cv2.medianBlur(img, 9)
-# find all the 'black' shapes in the image
-lower = np.array([0, 0, 0])
-upper = np.array([90, 90, 90])
-shapeMask = cv2.inRange(img, lower, upper)
-# find the contours in the mask
-(contours, _) = cv2.findContours(shapeMask.copy(), cv2.RETR_EXTERNAL,
-	cv2.CHAIN_APPROX_SIMPLE)
- 
-for cnt in contours:
-	area = cv2.contourArea(cnt)
-	#print area
+        img = image
+        img=cv2.medianBlur(img, 9)
+        # find all the 'black' shapes in the image
+        lower = np.array([0, 0, 0])
+        upper = np.array([90, 90, 90])
+        shapeMask = cv2.inRange(img, lower, upper)
+        # find the contours in the mask
+        (contours, _) = cv2.findContours(shapeMask.copy(), cv2.RETR_EXTERNAL,
+                cv2.CHAIN_APPROX_SIMPLE)
+         
+        for cnt in contours:
+                area = cv2.contourArea(cnt)
+                #print area
 
-	if area>1200 and area<7000:
-#		print area
-		x,y,w,h = cv2.boundingRect(cnt)
-		cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,255), 2)
-		img=img[y:y+h, x:x+w]
-		break
-lower = np.array([0, 0, 0])
-upper = np.array([90, 90, 90])
-shapeMask = cv2.inRange(img, lower, upper)
+                if area>1200 and area<7000:
+        #		print area
+                        x,y,w,h = cv2.boundingRect(cnt)
+                        cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,255), 2)
+                        img=img[y:y+h, x:x+w]
+                        break
+        lower = np.array([0, 0, 0])
+        upper = np.array([90, 90, 90])
+        shapeMask = cv2.inRange(img, lower, upper)
 
-# find all the 'black' shapes in the image
-k=35
-m=8
-n=8
-c=[]
-inputArray=[]
-s01_=shapeMask.shape[0]
-s02_=shapeMask.shape[1]
-#populating input array with random numbers 0 and 255 for testing purposes
-for i in range (s01_):
-	for j in range (s02_):
-		c.append(shapeMask[i][j])
-	inputArray.append(c)
-	c=[]
-outputArrayRows=(s01_/m)
-outputArrayColumns=(s02_/n)
-sre=''
-f = open(text_dir, 'w')
-for i in range (outputArrayRows):
-	for j in range (outputArrayColumns):
-		if countArray(i*m,j*n)>=k:
-			sre+='1'
-		else:
-			sre+='0'
-	f.write(sre+'\n')
-	sre=''
-f.close()
+        # find all the 'black' shapes in the image
+        k=35
+        m=8
+        n=8
+        c=[]
+        inputArray=[]
+        s01_=shapeMask.shape[0]
+        s02_=shapeMask.shape[1]
+        #populating input array with random numbers 0 and 255 for testing purposes
+        for i in range (s01_):
+                for j in range (s02_):
+                        c.append(shapeMask[i][j])
+                inputArray.append(c)
+                c=[]
+        outputArrayRows=(s01_/m)
+        outputArrayColumns=(s02_/n)
+        sre=''
+        f = open(text_dir, 'w')
+        for i in range (outputArrayRows):
+                for j in range (outputArrayColumns):
+                        if countArray(i*m,j*n)>=k:
+                                sre+='1'
+                        else:
+                                sre+='0'
+                f.write(sre+'\n')
+                sre=''
+        f.close()
