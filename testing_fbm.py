fbe7727e78c98a8f4d58cfb918f86125c87f5581
Ridhwan <ridhwancode@gmail.com>
2016-03-02 21:50:14 +0000

    Nothing new

--- testing_fbm.py
+++ testing_fbm.py
@@ -1,10 +1,4 @@
-import first_bot_movement as fbm
+import bot_movement as fbm
 import time as tm
 
-fbm.turn_right(90)
-tm.sleep(1)
-fbm.turn_right(90)
-tm.sleep(1)
-fbm.turn_right(90)
-tm.sleep(1)
-fbm.turn_right(90)
+fbm.first_up()
