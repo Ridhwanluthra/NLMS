5804d9a4a79c6ca392729320c56ebe68351367d3
pankajisadeveloper <pankajisadeveloper>
2016-03-03 19:07:23 +0530

    click_pic_final

--- click_picture.py
+++ click_picture.py
@@ -2,45 +2,20 @@
 import numpy as np
 import cv2
 import picamera
-import svmcode as svm
-
-
-m=8
-n=8
-inputArray=[]
  
 # construct the argument parse and parse the arguments
 # load the image
 
-
-def countArray(i, j):
-        global m
-        global n
-        global inputArray
-        counter=0
-        for row in range(i, i+m):
-                for column in range(j, j+n):
-                        if(inputArray[i][j]==255):
-                                counter+=1
-        return counter
-
-
 def clicked():
         direc='img.jpg'
-        text_dir='img_digit.txt'
         camera=picamera.PiCamera()
-        camera.vflip=True
-        camera.hflip=True
-#        camera.contrast=40
-        #camera.brightness=40
-        #camera.saturation=30 
         camera.resolution=(300,200)
+	from time import sleep
         camera.capture(direc)
-
+	sleep(1)
             #analysis of image
         # load the image
         image = cv2.imread(direc)
-
         img = image
         img=cv2.medianBlur(img, 9)
         # find all the 'black' shapes in the image
@@ -60,32 +35,29 @@ def clicked():
                         x,y,w,h = cv2.boundingRect(cnt)
                         cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,255), 2)
                         img=img[y:y+h, x:x+w]
+                        img = cv2.resize(img, (50, 200)) 
                         break
         lower = np.array([0, 0, 0])
         upper = np.array([60, 60, 60])
         shapeMask = cv2.inRange(img, lower, upper)
 
-        # find all the 'black' shapes in the image
-        k=35
-        c=[]
-        global m
-        global n
-        global inputArray
-        s01_=shapeMask.shape[0]
-        s02_=shapeMask.shape[1]
-        #populating input array with random numbers 0 and 255 for testing purposes
-        for i in range (s01_):
-                for j in range (s02_):
-                        c.append(shapeMask[i][j])
-                inputArray.append(c)
-                c=[]
-        outputArrayRows=(s01_/m)
-        outputArrayColumns=(s02_/n)
-        sre=''
-        for i in range (outputArrayRows):
-                for j in range (outputArrayColumns):
-                        if countArray(i*m,j*n)>=k:
-                                sre+='1'
-                        else:
-                                sre+='0'
-	print (svm.digit([sre]))
+        (contours, _) = cv2.findContours(shapeMask.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)
+        for cnt in contours:
+                area = cv2.contourArea(cnt)
+                if (area<5500 and area>4500):
+                        return 3
+                elif (area<7500 and area>6900):
+                        return 1
+                elif (area<6800 and area>6100):
+                        return 6
+                elif (area<3500 and area>2500):
+                        return 7
+                elif (area<9100 and area>8000):
+                        return 8
+                elif (area<3000 and area>2500):
+                        return 2
+                elif (area<2200 and area>1100):
+                        return 4
+                else:
+                        return 5
