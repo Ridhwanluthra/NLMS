582a6e5f62bd2f9be0d18edd856f970ac74984fb
pankajisadeveloper <pankajisadeveloper>
2016-03-03 22:41:20 +0530

    callibration corrected to be checked

--- callibration.py
+++ callibration.py
@@ -37,46 +37,53 @@ def angle_callibrate(read, distance):
     #center more than linear to incorporate the tires and ultra position
     center = 8
     distance_error = 2
-    angle_error = 1
+    angle_error = 2
 
     distance = distance + center
 
-    average_reading = (read[1] + read[2])/2
+    #average_reading = (read[1] + read[2])/2
 
-    difference = read[2] - read[1]
-    degree = asin(difference / sqrt(pow(ultra_diff, 2)+pow(difference, 2)))
-    degree = degrees(degree)
-
+    #difference = read[2] - read[1]
+    
+    """
     #correcting the distance from left
     if average_reading < distance - distance_error:
         # can add the below commented if angle correction wanted before
-        """if read[1] > read[2] + angle_error:
+        """"""if read[1] > read[2] + angle_error:
             bm.turn_left(degree)
         elif read[1] < read[2] - angle_error:
-            bm.turn_right(degree)"""
+            bm.turn_right(degree)""""""
         bm.turn_right(90)
         bm.move_forward(distance - average_reading)
         bm.turn_left(90)
             
     elif average_reading > distance + distance_error:
         # can add the below commented if angle correction wanted before
-        """if read[1] > read[2] + angle_error:
+        """"""if read[1] > read[2] + angle_error:
             bm.turn_left(degree)
         elif read[1] < read[2] - angle_error:
-            bm.turn_right(degree)"""
+            bm.turn_right(degree)""""""
         bm.turn_left(90)
         bm.move_forward(average_reading - distance)
         bm.turn_right(90)
-
+        """
     #now correcting the angle
     while read[1] > read[2] + angle_error or read[1] < read[2] - angle_error:
         if read[1] > read[2] + angle_error:
-            bm.turn_left(degree)
-            read = callibration_ultra()
+        	difference = read[1] - read[2]
+        	degree = asin(difference / sqrt(pow(ultra_diff, 2)+pow(difference, 2)))
+    		degree = degrees(degree)
+            if degree > 4:
+	            bm.turn_left(degree - 4)
+	            read = callibration_ultra()
             
         elif read[1] < read[2] - angle_error:
-            bm.turn_right(degree)
-            read = callibration_ultra()
+        	difference = read[2] - read[1]
+        	degree = asin(difference / sqrt(pow(ultra_diff, 2)+pow(difference, 2)))
+    		degree = degrees(degree)
+    		if degree > 4:
+	            bm.turn_right(degree - 4)
+	            read = callibration_ultra()
 
 def callibrate(rows, columns, cx, cy, mapp):
     # [0] = move_forward, [1] = left, [2] = back
@@ -91,61 +98,6 @@ def callibrate(rows, columns, cx, cy, mapp):
         read = callibration_ultra()
 
     grid_size = 30
-    
-    #for linear callibration
-    if bot.direction == 'n':
-        if cx == 0:
-            distance = 0
-        else:
-            found_obstacle = False
-            for i in range(1, cx):
-                if mapp[i][cy] == 1:
-                    found_obstacle = True
-                    distance = (cx - i - 1) * grid_size
-                    break
-            if found_obstacle==False:
-                distance = cx * grid_size
-        
-    elif bot.direction == 's':
-        if cx == rows:
-            distance = 0
-        else:
-            found_obstacle = False
-            for i in range(cx, rows):
-                if mapp[i][cy] == 1:
-                    found_obstacle = True
-                    distance = (i - cx - 1) * grid_size
-                    break
-            if found_obstacle==False:
-                distance = (rows - cx - 1) * grid_size
-        
-    elif bot.direction == 'w':
-        if cy == 0:
-            distance = 0
-        else:
-            found_obstacle = False
-            for i in range(1, cy):
-                if mapp[cx][i] == 1:
-                    found_obstacle = True
-                    distance = (cy - i - 1) * grid_size
-                    break
-            if found_obstacle==False:
-                distance = cy * grid_size
-
-    elif bot.direction == 'e':
-        if cy == columns:
-            distance = 0
-        else:
-            found_obstacle = False
-            for i in range(cy, columns):
-                if mapp[cx][i] == 1:
-                    found_obstacle = True
-                    distance = (i - cy - 1) * grid_size
-                    break
-            if found_obstacle==False:
-                distance = (columns - cy - 1) * grid_size
-    
-    linear_callibrate(readings[0], distance)
 
     # for angle callibration
     if bot.direction == 'n':
@@ -201,3 +153,58 @@ def callibrate(rows, columns, cx, cy, mapp):
                 distance = cx * grid_size
 
     angle_callibrate(readings, distance)
+
+    #for linear callibration
+    if bot.direction == 'n':
+        if cx == 0:
+            distance = 0
+        else:
+            found_obstacle = False
+            for i in range(1, cx):
+                if mapp[i][cy] == 1:
+                    found_obstacle = True
+                    distance = (cx - i - 1) * grid_size
+                    break
+            if found_obstacle==False:
+                distance = cx * grid_size
+        
+    elif bot.direction == 's':
+        if cx == rows:
+            distance = 0
+        else:
+            found_obstacle = False
+            for i in range(cx, rows):
+                if mapp[i][cy] == 1:
+                    found_obstacle = True
+                    distance = (i - cx - 1) * grid_size
+                    break
+            if found_obstacle==False:
+                distance = (rows - cx - 1) * grid_size
+        
+    elif bot.direction == 'w':
+        if cy == 0:
+            distance = 0
+        else:
+            found_obstacle = False
+            for i in range(1, cy):
+                if mapp[cx][i] == 1:
+                    found_obstacle = True
+                    distance = (cy - i - 1) * grid_size
+                    break
+            if found_obstacle==False:
+                distance = cy * grid_size
+
+    elif bot.direction == 'e':
+        if cy == columns:
+            distance = 0
+        else:
+            found_obstacle = False
+            for i in range(cy, columns):
+                if mapp[cx][i] == 1:
+                    found_obstacle = True
+                    distance = (i - cy - 1) * grid_size
+                    break
+            if found_obstacle==False:
+                distance = (columns - cy - 1) * grid_size
+    
+    linear_callibrate(readings[0], distance)
